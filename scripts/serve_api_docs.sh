21cfdfbb721647baf2f702a9032041fe754bd75f
Alex Strick van Linschoten <stricksubscriptions@fastmail.fm>
2023-04-19 15:27:37 +0100

    update naming

--- scripts/serve_api_docs.sh
+++ scripts/serve_api_docs.sh
@@ -6,7 +6,8 @@ SRC=${1:-"src/zenml"}
 
 export ZENML_DEBUG=1
 export ZENML_ANALYTICS_OPT_IN=false
-rm -rf docs/mkdocs/api_docs || true
+rm -rf docs/mkdocs/core_python_sdk_docs || true
+rm -rf docs/mkdocs/integration_code_docs || true
 rm docs/mkdocs/index.md || true
 
 python docs/mkdocstrings_helper.py --path $SRC --output_path docs/mkdocs/
